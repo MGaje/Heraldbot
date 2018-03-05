0e5788d60b9733ccda759c5baa469ea01fb14e71
MGajewski <gajewski.mike@gmail.com>
2018-03-05 14:50:44 -0500

    Phrase character correction, added bot name to constants, and implemented direct heraldbot communication behavior.

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,6 +1,9 @@
 // The Discord id of the bot.
 export const BotId: string = "418466140562522124";
 
+// Discord bot name.
+export const BotName: string = "heraldbot";
+
 // Collection of data store keys.
 export class DataStoreKeys
 {
--- src/handlers/MessageHandler.ts
+++ src/handlers/MessageHandler.ts
@@ -1,7 +1,7 @@
 import * as Discord from "discord.js";
 import * as Winston from "winston";
 
-import { DataStoreKeys } from "../core/constants";
+import { DataStoreKeys, BotName } from "../core/constants";
 import { Utility } from "../core/Utility";
 import { DataStore } from "../core/DataStore";
 
@@ -27,6 +27,24 @@ export class MessageHandler
      */
     public handleMsg(message: Discord.Message)
     {
+        const normalizedContent: string = message.content.toLowerCase();
+        if (normalizedContent === "hi heraldbot!")
+        {
+            // If someone is greeting heraldbot.
+            const guildMember: Discord.GuildMember = message.guild.members.find(x => x.id === message.author.id);
+            message.channel.send("hi " +  guildMember.displayName.toLowerCase() + "!");
+
+            return;
+        }
+        else if (message.content.toLowerCase().includes(BotName))
+        {
+            // If someone directly mentions heraldbot.
+            this.sayRandomPhrase(message.channel as Discord.TextChannel);
+            
+            return;
+        }
+
+        // Otherwise, there's a chance of heraldbot speaking regardless.
         const chanceBound: number = this._dataStore.get(DataStoreKeys.Chance);
         const chance: number = Utility.randomNumber(1, chanceBound);
         const chancePer: number = Math.round(100 * (1 / (chanceBound - 1)));
@@ -35,8 +53,17 @@ export class MessageHandler
 
         if (chance === 1)
         {
-            const corpus: string[] = this._dataStore.get(DataStoreKeys.Corpus);
-            message.channel.send(corpus[Utility.randomNumber(0, corpus.length)]);
+            this.sayRandomPhrase(message.channel as Discord.TextChannel);
         }        
+    }
+
+    /**
+     * Say a random phrase in the specified channel.
+     * @param {Discord.TextChannel} channel The Discord channel to say a random phrase in.
+     */
+    private sayRandomPhrase(channel: Discord.TextChannel)
+    {
+        const corpus: string[] = this._dataStore.get(DataStoreKeys.Corpus);
+        channel.send(corpus[Utility.randomNumber(0, corpus.length)]);
     }
 }
